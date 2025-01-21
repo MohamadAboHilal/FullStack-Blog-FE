4ea2c7d90cef86bdf190d5a557f3ff28b776d340
cvemir369 <cvemir369@gmail.com>
2025-01-21 22:33:56 +0100

    createPostPage, api.js, toasts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import AppRouter from "./router";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <div>
+      <Toaster />
       <AppRouter />
     </div>
   );
