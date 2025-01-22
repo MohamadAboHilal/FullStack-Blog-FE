0ebf2e1734dc3c81852b9620c9ca01e522392da2
cvemir369 <cvemir369@gmail.com>
2025-01-22 11:58:08 +0100

    hotfix for blogcard no cover img and api.js same code structure

--- src/services/api.js
+++ src/services/api.js
@@ -22,21 +22,12 @@ export const fetchPostById = async (id) => {
   }
 };
 
-/**
- * Create a new post.
- * @param {Object} postData - The data for the new post.
- * @param {string} postData.author - Author of the post.
- * @param {string} postData.title - Title of the post.
- * @param {string} postData.content - Content of the post.
- * @param {string} postData.cover - Cover image URL of the post.
- * @returns {Promise<Object>} - The response data from the server.
- */
 export const createPost = async (postData) => {
   try {
-    const response = await axios.post(`${VITE_BASE_URL}/posts`, postData);
+    const response = await api.post(`/posts`, postData);
     return response.data;
   } catch (error) {
-    throw error; // Rethrow the error so it can be handled by the calling code
+    throw error;
   }
 };
 
