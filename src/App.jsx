333f458ab362f14a56001c602fdfff27c18863f7
Paul Kim <ppaauullkkiimm@gmail.com>
2025-01-22 10:27:54 +0100

    Api, App, HomePage, BlogCard

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreatePostPage from "./pages/CreatePostPage";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create-post" element={<CreatePostPage />} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="navbar bg-base-100">
-      <div className="flex-1">
-        <Link to="/" className="text-3xl font-extrabold">
-          blogg.
-        </Link>
-      </div>
-      <div className="flex-none">
-        <Link to="/create-post" className="btn btn-neutral">
-          + Create Post
-        </Link>
+    <header className="navbar px-0 pt-4">
+      <div className="w-10/12 mx-auto">
+        <div className="flex-1">
+          <Link to="/" className="text-3xl font-extrabold">
+            blogg.
+          </Link>
+        </div>
+        <div className="flex-none">
+          <Link to="/create-post" className="btn btn-neutral">
+            + Create Post
+          </Link>
+        </div>
       </div>
     </header>
   );
